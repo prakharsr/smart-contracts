d23fb7fbbc52b5057809effb7cf3b86b89514a3d
prakharsr <prakhar03sharma@gmail.com>
2020-03-30 11:59:05 +0530

    remotepurc, simple trans

---
+++ safe remote purchase/saferemotepurchase.sol
@@ -0,0 +1,89 @@
+pragma solidity >0.4.23 <0.7.0;
+
+contract Purchase {
+    uint public value;
+    address payable public buyer;
+    address payable public seller;
+
+    enum State {Created, Locked, Release, Inactive }
+
+    State public state;
+
+    modifier condition(bool _condition) {
+        require(_condition);
+        _;
+    }
+
+    modifier onlyBuyer() {
+        require(msg.sender == buyer, "Only buyer can call this");
+        _;
+    }
+
+   modifier onlySeller() {
+        require(
+            msg.sender == seller,
+            "Only seller can call this."
+        );
+        _;
+    }
+
+    modifier inState(State _state) {
+        require(
+            state == _state,
+            "Invalid state."
+        );
+        _;
+    }
+
+    event Aborted();
+    event PurchaseConfirmed();
+    event ItemReceived();
+    event SellerRefunded();
+
+    constructor() public payable {
+        seller = msg.sender;
+        value = msg.value / 2;
+        require((2 * value) == msg.value, "Value has to be even.");
+    }
+
+    function abort()
+        public
+        onlySeller
+        inState(State.Created)
+    {
+        emit Aborted();
+        state = State.Inactive;
+        seller.transfer(address(this).balance);
+    }
+
+    function confirmPurchase()
+        public
+        inState(State.Created)
+        condition(msg.value == (2 * value))
+        payable
+    {
+        emit PurchaseConfirmed();
+        buyer = msg.sender;
+        state = State.Locked;
+    }
+
+    function confirmReceived()
+        public
+        onlyBuyer
+        inState(State.Locked)
+    {
+        emit ItemReceived();
+        state = State.Release;
+        buyer.transfer(value);
+    }
+
+    function refundSeller()
+        public
+        onlySeller
+        inState(State.Release)
+    {
+        emit SellerRefunded();
+        state = State.Inactive;
+        seller.transfer(3 * value);
+    }
+}
